4cfe2480c02ff29915ec00f399c7c587caab30b2
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-03-22 10:31:59 +0900

    Release v1.0.0-1.12

--- src/main/java/hiiragi283/lava_sweeper/LavaSweeper.java
+++ src/main/java/hiiragi283/lava_sweeper/LavaSweeper.java
@@ -3,27 +3,17 @@ package hiiragi283.lava_sweeper;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = "lava_sweeper", name = "LavaSweeper", version = "v0.1.0")
+@Mod(modid = "lava_sweeper", name = "LavaSweeper", version = "v1.0.0-1.12", acceptedMinecraftVersions = "[1.12, 1.12.2]")
 public class LavaSweeper {
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         //イベントを登録する
         MinecraftForge.TERRAIN_GEN_BUS.register(this);
-    }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-    }
-
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
     }
 
     @SubscribeEvent
