4a2f699792ee8f835d1cd176eff4c06f008e43a3
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-05-04 18:03:55 +0900

    Update scripts

--- src/main/java/hiiragi283/lava_sweeper/LavaSweeper.java
+++ src/main/java/hiiragi283/lava_sweeper/LavaSweeper.java
@@ -1,13 +1,15 @@
 package hiiragi283.lava_sweeper;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.terraingen.PopulateChunkEvent;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.eventhandler.Event;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.terraingen.PopulateChunkEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = "lava_sweeper", name = "LavaSweeper", version = "v1.0.0-1.7.10", acceptedMinecraftVersions = "[1.7.10]")
 public class LavaSweeper {
@@ -16,15 +18,15 @@ public class LavaSweeper {
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        //イベントを登録する
+        // イベントを登録する
         MinecraftForge.TERRAIN_GEN_BUS.register(this);
-        //ログを出力する
+        // ログを出力する
         LOG.info("LavaSweeper has been activated!");
     }
 
     @SubscribeEvent
     public void removeLavaTrap(PopulateChunkEvent.Populate event) {
-        //ネザーの1マス溶岩を消す
+        // ネザーの1マス溶岩を消す
         if (event.type == PopulateChunkEvent.Populate.EventType.NETHER_LAVA) {
             event.setResult(Event.Result.DENY);
         }
